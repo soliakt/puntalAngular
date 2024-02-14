d41690eb9e15ed1610c1083f6f074d2b0fcef8bc
Jr-JDP <jdp11037@gmail.com>
2024-02-14 13:16:18 +0100

    Componentes faltantes 3

---
+++ src/app/components/mobile-form/mobile-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mobile-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './mobile-form.component.html',
+  styleUrl: './mobile-form.component.css'
+})
+export class MobileFormComponent {
+
+}
--- src/app/components/mobile-table/mobile-table.component.ts
+++ src/app/components/mobile-table/mobile-table.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ElementRef, Renderer2 } from '@angular/core';
+import { Generic } from '../../interfaces/generic';
+import { GenericsService } from '../../services/generics/generics.service';
 
 @Component({
   selector: 'app-mobile-table',
@@ -7,6 +9,30 @@ import { Component } from '@angular/core';
   templateUrl: './mobile-table.component.html',
   styleUrl: './mobile-table.component.css'
 })
-export class MobileTableComponent {
+export class MobileTableComponent implements OnInit{
+  //Construir array de la interfaz
+  generics: Generic[] =[];
+
+  constructor(private genericsService: GenericsService, private renderer: Renderer2, private el: ElementRef) {
+    this.recover();
+  }
+
+  ngOnInit() {
+    this.loadTableJS();
+  }
+
+  recover() {
+    this.genericsService.returnService().subscribe((generics: Generic[]) => {
+      this.generics = generics
+    });
+  }
+
+  loadTableJS() {
+    const script = this.renderer.createElement('script');
+    script.type = 'text/javascript';
+    script.src = './assets/javascript/mobile-table.js';
+    script.defer = true;
+    this.renderer.appendChild(this.el.nativeElement, script);
+  }
 
 }
