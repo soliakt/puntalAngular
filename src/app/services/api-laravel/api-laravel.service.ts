cf1e1225fc12f5a5b9462bb1f5db58230cebd56c
DavidGuillenGarcia <dguillengar@gmail.com>
2024-02-18 22:13:54 +0100

    Endpoint logs

--- src/app/services/api-laravel/api-laravel.service.ts
+++ src/app/services/api-laravel/api-laravel.service.ts
@@ -1,14 +1,21 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class ApiLaravelService {
-    private defaultURL = "http://prodeveloplaravel.test/api";
-    constructor(private http: HttpClient) { }
-    getReservationInfoFiltered(): Observable<any[]> {
-      return this.http.get<any[]>(`${this.defaultURL}/reservation/all/getReservationInfoFiltered`);
+  private defaultURL = "http://127.0.0.1:8000/api";
+  constructor(private http: HttpClient) {}
+  getReservationInfoFiltered(): Observable<any[]> {
+    return this.http.get<any[]>(
+      `${this.defaultURL}/reservation/all/getReservationInfoFiltered`
+    );
+  }
+  getLogsFiltered(): Observable<any[]> {
+    return this.http.get<any[]>(
+      `${this.defaultURL}/reservation/all/getLogsFiltered`
+    );
   }
 }
