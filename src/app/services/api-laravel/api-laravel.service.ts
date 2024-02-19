416f745815befecbf1cef5ecb0ea65f11bb2ea91
NichikaZ <oscarylios25@gmail.com>
2024-02-19 10:32:48 +0100

    Revert "Merge pull request #12 from Prodevelop2DAW/david"
    This reverts commit b7a06fe55edbf5b64dfe30f2f6f5af83e0a71876, reversing
    changes made to 0d5d2c7a4159db06e2bafecaa61be0f7f7846851.

--- src/app/services/api-laravel/api-laravel.service.ts
+++ src/app/services/api-laravel/api-laravel.service.ts
@@ -1,21 +1,19 @@
-import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
-import { Observable } from "rxjs";
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: "root",
+  providedIn: 'root'
 })
 export class ApiLaravelService {
-  private defaultURL = "http://127.0.0.1:8000/api";
-  constructor(private http: HttpClient) {}
-  getReservationInfoFiltered(): Observable<any[]> {
-    return this.http.get<any[]>(
-      `${this.defaultURL}/reservation/all/getReservationInfoFiltered`
-    );
-  }
-  getLogsFiltered(): Observable<any[]> {
-    return this.http.get<any[]>(
-      `${this.defaultURL}/reservation/all/getLogsFiltered`
-    );
-  }
+    private defaultURL = "http://127.0.0.1:8000/api";
+    constructor(private http: HttpClient) { }
+
+    getReservationInfoFiltered(): Observable<any[]> {
+      return this.http.get<any[]>(`${this.defaultURL}/reservation/all/getReservationInfoFiltered`);
+    }
+
+    updateReservationConfirmation(reservationId: number): Observable<any> {
+      return this.http.put<any>(`${this.defaultURL}/reservation/update/status/${reservationId}`, {});
+    }
 }
--- src/app/users/aduana/aduana-table/aduana-table.component.ts
+++ src/app/users/aduana/aduana-table/aduana-table.component.ts
@@ -1,69 +1,43 @@
-import { Component, OnInit, ElementRef, Renderer2 } from "@angular/core";
+import { Component, OnInit, ElementRef, Renderer2 } from '@angular/core';
 
 //Importar la interfaz y el servicio
-import { Generic } from "../../../interfaces/generic";
-import { GenericsService } from "../../../services/generics/generics.service";
-import { RouterOutlet } from "@angular/router";
-import { Subscription } from "rxjs";
-import { ApiLaravelService } from "../../../services/api-laravel/api-laravel.service";
-import { MobileSectionService } from "../../../services/mobile-section/mobile-section.service";
+import { Generic } from '../../../interfaces/generic';
+import { GenericsService } from '../../../services/generics/generics.service';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: "app-aduana-table",
+  selector: 'app-aduana-table',
   standalone: true,
   imports: [RouterOutlet],
-  templateUrl: "./aduana-table.component.html",
-  styleUrl: "./aduana-table.component.css",
+  templateUrl: './aduana-table.component.html',
+  styleUrl: './aduana-table.component.css',
 })
-export class AduanaTableComponent implements OnInit {
+export class AduanaTableComponent {
   //Construir array de la interfaz
-  data: any[] = [];
-  selectedItem: string | undefined;
-  private subscription: Subscription = new Subscription();
+  generics: Generic[] = [];
 
   constructor(
     private genericsService: GenericsService,
     private renderer: Renderer2,
-    private el: ElementRef,
-    private apiLaravelService: ApiLaravelService,
-    private mobilesectionService: MobileSectionService
+    private el: ElementRef
   ) {
     this.recover();
   }
 
   ngOnInit() {
-    this.subscribeToSelectedItem();
-    this.recover();
     this.loadTableJS();
   }
-  subscribeToSelectedItem() {
-    this.subscription = this.mobilesectionService.selectedItem$.subscribe(
-      (item) => {
-        this.selectedItem = item;
-        this.recover();
-      }
-    );
-  }
+
   recover() {
-    if (this.selectedItem === "Entradas") {
-      this.apiLaravelService.getLogsFiltered().subscribe((data: any[]) => {
-        this.data = data.filter((item) => !item.date_entry_confirmed);
-      });
-    } else if (this.selectedItem === "Salidas") {
-      this.apiLaravelService.getLogsFiltered().subscribe((data: any[]) => {
-        this.data = data.filter((item) => !item.date_exit_confirmed);
-      });
-    } else {
-      this.apiLaravelService.getLogsFiltered().subscribe((data: any[]) => {
-        this.data = data;
-      });
-    }
+    this.genericsService.returnService().subscribe((generics: Generic[]) => {
+      this.generics = generics;
+    });
   }
 
   loadTableJS() {
-    const script = this.renderer.createElement("script");
-    script.type = "text/javascript";
-    script.src = "./assets/javascript/table.js";
+    const script = this.renderer.createElement('script');
+    script.type = 'text/javascript';
+    script.src = './assets/javascript/table.js';
     script.defer = true;
     this.renderer.appendChild(this.el.nativeElement, script);
   }
